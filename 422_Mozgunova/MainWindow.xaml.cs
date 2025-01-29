5507bcfee7391cbaacb49e4ac0f0eaccc49ece61
dariamozgunova <savemyselfkm@gmail.com>
2025-01-29 15:14:52 +0300

    ПР1 завершена.

--- 422_Mozgunova/MainWindow.xaml.cs
+++ 422_Mozgunova/MainWindow.xaml.cs
@@ -24,5 +24,72 @@ namespace _422_Mozgunova
         {
             InitializeComponent();
         }
+
+        private void ButtonCalculator(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(input_x.Text) || string.IsNullOrWhiteSpace(input_p.Text))
+            {
+                MessageBox.Show("Поля x и/или p НЕ заполнены значениями, поэтому вычисление невозможно!");
+                return;
+            }
+
+            if (!double.TryParse(input_x.Text, out double x))
+            {
+                MessageBox.Show("Некорректное значение x");
+                return;
+            }
+
+            if (!double.TryParse(input_p.Text, out double p))
+            {
+                MessageBox.Show("Некорректное значение p");
+                return;
+            }
+
+            double fx;
+            if (radioSh.IsChecked == true)
+            {
+                fx = Math.Sinh(x);
+            }
+            else if (radioX2.IsChecked == true)
+            {
+                fx = Math.Pow(x, 2);
+            }
+            else
+            {
+                fx = Math.Exp(x);
+            }
+
+            double abs_p = Math.Abs(p);
+            double l = 0;
+            if (x >  abs_p) 
+            {
+                l = 2 * Math.Pow(fx, 3) + 3 * Math.Pow(p, 2);
+            }
+            else if (x > 3 && x < abs_p)
+            {
+                l = Math.Abs(fx - p);
+            }
+            else
+            {
+                l = Math.Pow(fx - p, 2);
+            }
+            result.Text = l.ToString();
+        }
+
+        private void ButtonClear(object sender, RoutedEventArgs e)
+        {
+            input_x.Text = "";
+            input_p.Text = "";
+            result.Text = "";
+            radioSh.IsChecked = true;
+        }
+
+        private void CloseWindow(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (MessageBox.Show("Хотите выйти?", "Подтверждение выхода", MessageBoxButton.YesNo) == MessageBoxResult.No)
+            {
+                e.Cancel = true;
+            }
+        }
     }
 }
